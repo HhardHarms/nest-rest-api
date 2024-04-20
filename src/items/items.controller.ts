ed9b76917fd2f7866b9760cba0d1c0b81a4eb2cc
Godwin A. Ndem <136455877+HhardHarms@users.noreply.github.com>
2024-04-20 17:01:57 +0100

    My second commit

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,26 +1,32 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor (private readonly itemsService: ItemsService) {}
+ 
   @Get()
-  findAll(): string {
-    return 'Get all items';
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item ${id}`;
+  async findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto)
   }
 
   @Delete(':id')
-  delete(@Param('id') id): string {
+  delete(@Param('id') _id): string {
     return 'Delete ${id}'
   }
 
